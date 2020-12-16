a847249a31f40eacb7aec662a927d45ee81bdffd
Andrew <Andrew@AndrewPC>
2020-12-15 21:31:39 -0500

    Changing window details for new user scene

--- src/main/java/edu/psu/bw/hallettist261project/NewUserController.java
+++ src/main/java/edu/psu/bw/hallettist261project/NewUserController.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -53,13 +54,28 @@ public class NewUserController {
     Stage homeScreenStage;
 
     @FXML
+    private Button returnNewUserButton;
+
+    @FXML
     void handleCreateNewUserButton(ActionEvent event) {
 
     }
 
     @FXML
-    void handleReturnNewUserOnAction(ActionEvent event) throws IOException {
-        
-        
+    void handleReturnButtonOnAction(ActionEvent event) throws IOException {
+        System.out.println("----------LOGIN BUTTON PRESSED----------");
+        //Loading the new screen once the login button is pressed
+        Parent newUserParent = FXMLLoader.load(getClass().getResource("/fxml/LoginScene.fxml"));
+        Scene newUserScene = new Scene(newUserParent);
+        //Getting stage information 
+        Stage newUserStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        //Showing the scene
+        newUserStage.setScene(newUserScene);
+        newUserStage.show();
+        //Setting scene to display in center of screen
+        newUserStage.centerOnScreen();
+        //Disabling resizable
+        newUserStage.resizableProperty().setValue(Boolean.FALSE);
+
     }
 }
